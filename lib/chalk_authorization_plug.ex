1f3ebbfd7438e9495d26d53cef6b090ab6f7b21f
Manlio Joaquín García González <info@manliogarcia.es>
2022-01-25 18:22:36 +0000

    Clean style

--- lib/chalk_authorization_plug.ex
+++ lib/chalk_authorization_plug.ex
@@ -8,13 +8,15 @@ defmodule ChalkAuthorization.Plug do
     |> redirect(to: opts[:bounce_to] || "/")
     |> halt()
 
-  def require_authorization(%{assigns: %{current_user: %{superuser: true}}} = conn, _opts \\ []),
+  def require_authorization(conn, opts \\ [])
+
+  def require_authorization(%{assigns: %{current_user: %{superuser: true}}} = conn, _opts),
     do: conn
 
-  def require_authorization(%{assigns: %{current_user: %{__struct__: struct} = user}} = conn, [permission: {action, element}] = opts),
+  def require_authorization(%{assigns: %{current_user: %{__struct__: struct} = user}} = conn, [permission: {action, element}]),
     do: if(struct.can?(user, action, element), do: conn, else: unauthorize(conn))
 
-  def require_authorization(%{assigns: %{current_user: %{__struct__: struct} = user}} = conn, [group: group] = opts),
+  def require_authorization(%{assigns: %{current_user: %{__struct__: struct} = user}} = conn, [group: group]),
     do: if(struct.is_a?(user, group), do: conn, else: unauthorize(conn))
 
   def require_authorization(conn, _opts),
